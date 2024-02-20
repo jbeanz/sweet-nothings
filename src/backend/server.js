3ac7de69ba61a5ed1984a901395f92aed2aab2b4
Joseph Triolo <joseph.triolo2@gmail.com>
2024-02-20 18:29:52 -0500

    Add middleware to server

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,11 +1,27 @@
 const express = require('express')
 
+const { sendMessage } = require('./sendMessage')
+
 const PORT = 8080
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
+app.use(express.json())
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+app.use(
+    (req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*')
+        res.header('Access-Control-Allow-Headers', '*')
+        next()
+  }
+)
+
+app.post('/sendMessage', (req, res) => {
+    sendMessage(req.body)
 })
 
 app.listen(PORT, () => {
